dbbc471904655ee0ff54e71bb36d89e6775de092
tudora9008 <dtuni9008@outlook.com>
2023-12-20 21:15:54 +0200

    Add project files.

---
+++ TudoraDragosPaulLaborator7/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace TudoraDragosPaulLaborator7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ TudoraDragosPaulLaborator7/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TudoraDragosPaulLaborator7.Data;
+using System.IO;
+
+
+namespace TudoraDragosPaulLaborator7
+{
+    public partial class App : Application
+    {
+
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                    ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                    LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
